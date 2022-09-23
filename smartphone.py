fbb75ae4e0f71aedcf34efb4916452e8aec4b180
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2022-09-23 11:51:36 +0200

    uitbreiding1

--- smartphone.py
+++ smartphone.py
@@ -1,5 +1,4 @@
 
-
 def cheap_phone(cheap_phone , cheap_price ):
     return  "De {} is het goedKoopst, de telefoon kost {} Euro".format(cheap_phone  , cheap_price )
 
@@ -9,24 +8,38 @@ def expensive_phone(expensive_phone ,  expensive_price ):
 def result( cheap_phone , expensive_phone , deffrince ):
     return "Het advies is dus de {} te kopen. Deze is namelijk {} euro goedkoper dan {}".format(cheap_phone , deffrince , expensive_phone )
 
+def diffrence(num1 ,num2):
+    return num1 - num2
+
 
 def advise(iphone , samsung  ):
+    if iphone  > 900 and samsung > 900:
+        return "Het advies is dus geen telefoon te kopen , ze zijn te duur"
+    
+    elif iphone > 900 :
+       
+        return result( "samsung" , "iphone" ,  diffrence(iphone  , samsung) )
+    
+    elif samsung > 900 :
+       
+        return result( "iphone" , "samsung"  ,  diffrence( samsung , iphone) )
 
-    if iphone > samsung:
-
-        deffrince = iphone - samsung
-
-        if deffrince > 50 :
-            return result( "samsung" , "iphone" , deffrince )
-            
-        else:
-            return "Het advies is dus de iphone te kopen. Deze is namelijk {} euro duurder dan samsung".format( deffrince )
-            
     else:
-        deffrince =  samsung - iphone
-        return result( "iphone" , "samsung" , deffrince )
-        
+        if iphone > samsung:
 
+
+            deffrince =  diffrence( iphone , samsung )  
+
+            if deffrince > 50 :
+                return result( "samsung" , "iphone"  ,deffrince )
+                
+            else:
+                return "Het advies is dus de iphon te kopen. Deze is namelijk {} euro duurder dan samsung".format(deffrince )
+                
+        else:
+            deffrince = diffrence( samsung , iphone )  
+            return result( "iphone" , "samsung" , deffrince )
+        
 
 iphone = float(input("Hoe duur is de iphone  : "))
 samsung = float(input("Hoe duur is de samsung  : "))
